49eb2ef7ef4f64c04efb3ba916b389b627156f15
melesedadi <dadimeles@yahoo.com>
2019-09-26 16:29:31 -0400

    First Commit

---
+++ src/main/java/com/example/demo/HomeController.java
@@ -0,0 +1,44 @@
+package com.example.demo;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+    @RequestMapping("/")
+    public String home(){
+        return "home";
+    }
+
+    @RequestMapping("/coreJava")
+    public String coreJava(){
+        return "CoreJava";
+    }
+    @RequestMapping("/html")
+    public String html(){
+        return "html";
+    }
+
+    @RequestMapping("/bootStrap")
+    public String bootStrap(){
+        return "bootStrap";
+    }
+    @RequestMapping("/OOP")
+    public String OOP(){
+        return "OOP";
+    }
+    @RequestMapping("/gitHub")
+    public String gitHub (){
+        return "gitHub";
+    }
+    @RequestMapping("/bootcamp")
+    public String bootcamp (){
+        return "bootcamp";
+    }
+    @RequestMapping("/author")
+    public String author (){
+        return "author";
+    }
+}
+
